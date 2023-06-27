7f99c17b9861e39fb94438f6336cc8d1455af741
afeiship <1290657123@qq.com>
2023-06-28 07:27:36 +0800

    feat: add sync/aysnc fn

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,7 +1,58 @@
-import fn from '../src';
+import pipe from '../src';
 
-describe('api.basic', () => {
-  test('normail single value case', () => {
-    expect(fn({ rel: true })).toBe(undefined);
+const pipeSync = pipe.sync;
+const pipeAync = pipe.async;
+
+describe('Test for pipe', () => {
+  test('All sync functions', async () => {
+    const add = (x: number) => x + 1;
+    const double = (x: number) => x * 2;
+    const addThenDouble = pipeAync(add, double);
+    expect(await addThenDouble(1)).toBe(4);
+  });
+
+  test('All async functions', async () => {
+    const mockFetch = (x: number) => Promise.resolve(x + 1);
+    const mockDouble = (x: number) => Promise.resolve(x * 2);
+    const addThenDouble = pipeAync(mockFetch, mockDouble);
+    expect(await addThenDouble(1)).toBe(4);
+  });
+
+  test('Mixed sync and async functions', async () => {
+    const mockFetch = (x: number) => Promise.resolve(x + 1);
+    const mockDouble = (x: number) => x * 2;
+    const addThenDouble = pipeAync(mockFetch, mockDouble);
+    expect(await addThenDouble(1)).toBe(4);
+  });
+
+  test('Mixed with error', async () => {
+    const mockFetch = (x: number) => Promise.resolve(x + 1);
+    const mockDouble = (x: number) => x * 2;
+    const mockError = (x: number) => {
+      throw new Error('mock error');
+    };
+    const addThenDouble = pipeAync(mockFetch, mockError, mockDouble);
+    expect(await addThenDouble(1)).toBe(4);
+  });
+});
+
+describe('Test for pipe.sync', () => {
+  test('All sync functions', () => {
+    const add = (x: number) => x + 1;
+    const double = (x: number) => x * 2;
+    const addThenDouble = pipeSync(add, double);
+    expect(addThenDouble(1)).toBe(4);
+  });
+
+  test('Mixed with error', () => {
+    const mockDouble = (x: number) => x * 2;
+    const mockError = (x: number) => {
+      throw new Error('mock error');
+    };
+    const mockAddTwo = (x: number) => x + 2;
+    const addThenDouble1 = pipeSync(mockError, mockDouble, mockAddTwo);
+    const addThenDouble2 = pipeSync(mockDouble, mockError, mockAddTwo);
+    expect(addThenDouble1(1)).toBe(4);
+    expect(addThenDouble2(1)).toBe(4);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,42 @@
-const Pipe = (): void => {
-  console.log('hello');
-};
+declare var wx: any;
+
+type PipeAsyncFunction<T> = (input: T) => T | Promise<T>;
+type PipeSyncFunction<T> = (input: T) => T;
+
+function pipe<T>(...functions: PipeSyncFunction<T>[]): PipeSyncFunction<T> {
+  return function (input: T): T {
+    return functions.reduce((output: T, func: PipeSyncFunction<T>) => {
+      try {
+        return func(output);
+      } catch (error: any) {
+        console.log('Function execution skipped:', error);
+        return output;
+      }
+    }, input);
+  };
+}
+
+function pipeAsync<T>(...functions: PipeAsyncFunction<T>[]): PipeAsyncFunction<T> {
+  return async function (input: T): Promise<T> {
+    return functions.reduce(async (acc: Promise<T>, func: PipeAsyncFunction<T>) => {
+      try {
+        const output = await acc;
+        const result = await Promise.resolve(func(output));
+        return result;
+      } catch (error: any) {
+        console.log('Function execution skipped:', error.message);
+        return acc;
+      }
+    }, Promise.resolve(input));
+  };
+}
+
+pipe.async = pipeAsync;
+pipe.sync = pipe;
 
 // for commonjs es5 require
 if (typeof module !== 'undefined' && module.exports && typeof wx === 'undefined') {
-  module.exports = Pipe;
+  module.exports = pipe;
 }
 
-export default Pipe;
+export default pipe;
